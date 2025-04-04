6a2d2f959042ae0e2d89625b69e06fa45ff6a546
3027004969 <luo.junming@iwhalecloud.com>
2025-04-04 20:03:50 +0800

    #user模块实现登录以及注册功能

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.example.user.controller;
 
+import com.example.user.entity.User;
+import com.example.user.service.IUserService;
+import com.example.user.util.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    IUserService userService;
+
+    @GetMapping("/login")
+    public ResponseResult login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        return userService.login(username, password);
+    }
+
+    @GetMapping("/register")
+    public ResponseResult register(@RequestBody User user) {
+        System.out.println(user);
+        return userService.register(user);
+    }
 }
